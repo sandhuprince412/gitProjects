10f0ec4c66cb41a58f6a2d9eb89b9176a49b7f85
Prince Pal Singh <sandhuprince412@gmail.com>
2024-05-22 23:27:26 -0700

    Learning Passing data in EJS

---
+++ BackEnd/4.2 Passing Data/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { name } from "ejs";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs")
+});
+
+app.post("/submit", (req, res) => {
+  const numblength = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs",{
+nlength: numblength,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
