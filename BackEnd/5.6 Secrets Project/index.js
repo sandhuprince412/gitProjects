eef57b27c64139b8ce58980715c4b3908098a67e
Prince Pal Singh <sandhuprince412@gmail.com>
2024-05-29 00:28:22 -0700

    Secret Project

---
+++ BackEnd/5.6 Secrets Project/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import axios from "axios";
+
+const port = 3000;
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  const response = await axios.get("https://secrets-api.appbrewery.com/random");
+  const result = response.data;
+  const newSecret = result.secret;
+  const newUser = result.username;
+  res.render("index.ejs", { secret: newSecret, user: newUser });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at port ${port}`);
+});
