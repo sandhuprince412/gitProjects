904d18f40e95fa8c0dfff72c79874a180c6ee459
Prince Pal Singh <sandhuprince412@gmail.com>
2024-05-29 00:36:35 -0700

    Added Try..catch to Secret Project

--- BackEnd/5.6 Secrets Project/index.js
+++ BackEnd/5.6 Secrets Project/index.js
@@ -7,11 +7,18 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  const response = await axios.get("https://secrets-api.appbrewery.com/random");
-  const result = response.data;
-  const newSecret = result.secret;
-  const newUser = result.username;
-  res.render("index.ejs", { secret: newSecret, user: newUser });
+  try {
+    const response = await axios.get(
+      "https://secrets-api.appbrewery.com/random"
+    );
+    const result = response.data;
+    const newSecret = result.secret;
+    const newUser = result.username;
+    res.render("index.ejs", { secret: newSecret, user: newUser });
+  } catch (error) {
+    console.log(error.response.data);
+    res.status(500);
+  }
 });
 
 app.listen(port, () => {
