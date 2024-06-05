4cff557be94fc233be1c9608788140bd2deaff47
Prince Pal Singh <sandhuprince412@gmail.com>
2024-06-04 23:47:32 -0700

    Family travel tracker

---
+++ BackEnd/8.5 Family Travel Tracker/index.js
@@ -0,0 +1,99 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "World",
+  password: "$CANAda007$",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentUserId = 1;
+
+let getUsers = await db.query("select * from users");
+let users = getUsers.rows;
+
+async function checkVisited() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+app.get("/", async (req, res) => {
+  const countries = await checkVisited();
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: users,
+    color: users[0].color,
+  });
+});
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
+    );
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1,$2)",
+        [countryCode, currentUserId]
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.post("/user", async (req, res) => {
+  console.log(req.body.user);
+  currentUserId = req.body.user;
+  const result = await db.query("select * from users where id = $1", [
+    currentUserId,
+  ]);
+  const users = result.rows;
+  const result1 = await db.query(
+    "SELECT country_code FROM visited_countries where user_id = $1",
+    [currentUserId]
+  );
+  let countries = [];
+  result1.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(users);
+  console.log(users[0].color);
+  console.log(countries);
+  res.render("index.ejs", {
+    countries: countries,
+    total: countries.length,
+    users: users,
+    color: users[0].color,
+  });
+});
+
+app.post("/new", async (req, res) => {
+  //Hint: The RETURNING keyword can return the data that was inserted.
+  //https://www.postgresql.org/docs/current/dml-returning.html
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
