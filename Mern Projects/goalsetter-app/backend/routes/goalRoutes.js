9de100132bc33a1a880e13dcf212383fbcaccd9e
Prince Pal Singh <sandhuprince412@gmail.com>
2024-06-23 20:53:08 -0700

    Rest API for goals

--- Mern Projects/goalsetter-app/backend/routes/goalRoutes.js
+++ Mern Projects/goalsetter-app/backend/routes/goalRoutes.js
@@ -1,18 +1,49 @@
 import express from "express";
+import Goal from "../model/goalModel.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ message: "Get Goals" });
+router.get("/", async (req, res) => {
+  const goals = await Goal.find();
+  res.json(goals);
 });
-router.post("/", (req, res) => {
-  res.json({ message: "Set Goal" });
+router.post("/", async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field.");
+  }
+
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
+  res.json(goal);
 });
-router.put("/:id", (req, res) => {
-  res.json({ message: `update Goal ${req.params.id}` });
+router.put("/:id", async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.json(updatedGoal);
 });
-router.delete("/:id", (req, res) => {
-  res.json({ message: `update Goal ${req.params.id}` });
+router.delete("/:id", async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new error("Goal not found");
+  }
+
+  await Goal.findByIdAndDelete(req.params.id);
+
+  res.json({ message: `deleted Goal ${req.params.id}` });
 });
 
 export default router;
--- Mern Projects/goalsetter-app/backend/server.js
+++ Mern Projects/goalsetter-app/backend/server.js
@@ -1,9 +1,15 @@
 import express, { Router } from "express";
 //import dotenv from ("dotenv").config();
 import router from "./routes/goalRoutes.js";
+import connectDB from "./config/db.js";
+
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", router);
 
