0fdb88367595036e69830d9a157598c516ad8e29
Prince Pal Singh <sandhuprince412@gmail.com>
2024-07-13 15:44:09 -0700

    designing home and about page

---
+++ Portfolio-website/index.js
@@ -0,0 +1,12 @@
+var tabcontents = document.getElementsByClassName("tab-contents");
+var tablinks = document.getElementsByClassName("tab-links");
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
