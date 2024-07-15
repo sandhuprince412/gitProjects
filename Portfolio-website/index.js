d3db7e1f0e64e2db96f140adda56fa9c03e21ac7
Prince Pal Singh <sandhuprince412@gmail.com>
2024-07-15 10:21:49 -0700

    Adding mobile layout and form functionality

--- Portfolio-website/index.js
+++ Portfolio-website/index.js
@@ -1,5 +1,7 @@
-var tabcontents = document.getElementsByClassName("tab-contents");
-var tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+let tablinks = document.getElementsByClassName("tab-links");
+let sideMenu = document.getElementById("sidemenu");
+let msg = document.getElementById("msg");
 function opentab(tabname) {
   for (tablink of tablinks) {
     tablink.classList.remove("active-link");
@@ -10,3 +12,26 @@ function opentab(tabname) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
+function openMenu() {
+  sideMenu.style.right = "0";
+}
+function closeMenu() {
+  sideMenu.style.right = "-200px";
+}
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzfAZlesU6ooitNGS78YE1gk3TFEm4pl2FTx1g_l8impxDLYsxLNq3huZkxdjBriKxK/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(() => {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
