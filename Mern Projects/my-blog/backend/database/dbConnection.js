cbd95bc235010372c6055822c33835f152c68b34
Prince Pal Singh <sandhuprince412@gmail.com>
2024-07-27 22:54:59 -0700

    Changed from cloud to local

--- Mern Projects/my-blog/backend/database/dbConnection.js
+++ Mern Projects/my-blog/backend/database/dbConnection.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnection = () => {
   mongoose
-    .connect(
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.gmohkfv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-    )
+    .connect(`mongodb://localhost:27017/my-blog`)
     .then(() => {
       console.log("successfully connected to Database");
     })
